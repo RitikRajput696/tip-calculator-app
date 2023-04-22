6faca052d58d010ae06f5c4b9f56c90efef2cd8e
ritikrajput696 <rixusinput696@gmail.com>
2023-04-23 02:59:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const totalAmount = document.getElementById('total-amount').value;
+const perPerson = document.querySelector('#per-person');
+
+perPerson.textContent = totalAmount;
+console.log(totalAmount);
+console.log('fine');

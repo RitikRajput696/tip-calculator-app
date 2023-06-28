f6fb14e8789630d0c35ad3eb6868325b1ec248c2
RitikRajput696 <rixusinput696@gmail.com>
2023-06-29 03:11:03 +0530

    added input selector commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 // input buttons selector
-// const 
+const inputTotalNumber = document.querySelector("#input-total-amount");
+const btn5 = document.querySelector(".btn-5");
+const btn10 = document.querySelector(".btn-10");
+const btn15 = document.querySelector(".btn-15");
+const btn25 = document.querySelector(".btn-25");
+const btn50 = document.querySelector(".btn-50");
+const btnCustom = document.querySelector(".btn-custom");
+const tipBtns = document.querySelector(".btn-tip");
+const totalPeople = document.querySelector("#input-total-people");

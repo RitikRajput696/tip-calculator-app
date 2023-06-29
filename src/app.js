5b7155abc66335c194c9150c116bd33a54b557b6
RitikRajput696 <rixusinput696@gmail.com>
2023-06-29 22:48:18 +0530

    some additional commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // input buttons selector
-const inputTotalAmount = document.querySelector("#input-total-amount");
+const inputTotalAmount = document.getElementById("input-total-amount");
 const btn5 = document.querySelector(".btn-5");
 const btn10 = document.querySelector(".btn-10");
 const btn15 = document.querySelector(".btn-15");
@@ -7,10 +7,54 @@ const btn25 = document.querySelector(".btn-25");
 const btn50 = document.querySelector(".btn-50");
 const btnCustom = document.querySelector(".btn-custom");
 const tipBtns = document.querySelector(".btn-tip");
-const totalPeople = document.querySelector("#input-total-people");
+const inputTotalPeople = document.getElementById("input-total-people");
 
 // output buttons selector 
-const outputPayableAmount = document.querySelector("#total-payable-amount");
-const tipPerPerson = document.querySelector("#tip-amount");
-const totalPerPerson = document.querySelector("#per-person");
+const outputPayableAmount = document.getElementById("total-payable-amount");
+const tipPerPerson = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("per-person");
 const resetBtn = document.querySelector(".reset");
+
+// error
+const errorText = document.querySelector(".error");
+
+let totalAmount;
+let totalPeople;
+inputTotalAmount.addEventListener("change", () => {
+    let totalAmount = inputTotalAmount.value;
+    console.log(totalAmount);
+    if (totalAmount.value != 0) {
+        calculateBill();
+
+    }
+});
+
+inputTotalPeople.addEventListener("change", () => {
+    let totalPeople = inputTotalPeople.value;
+    console.log(totalPeople);
+    calculateBill();
+})
+
+function calculateBill() {
+    handleErrors();
+    setValues();
+}
+function handleErrors() {
+    if (totalAmount == 0 || totalAmount == "") {
+        errorText.classList.add('show-error');
+    } else {
+        errorText.classList.remove(".show-error");
+    }
+    if (totalPeople == 0 || totalPeople == "") {
+        errorText.classList.add('show-error');
+    } else {
+        errorText.classList.remove(".show-error");
+    }
+}
+
+function setValues() {
+    console.log(totalAmount);
+    console.log(totalPeople);
+    console.log(outputPayableAmount.textContent = totalAmount / totalPeople);
+    outputPayableAmount.textContent = totalAmount / totalPeople;
+}

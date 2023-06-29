125b734cd2eed9d9de72df31ce1de1a96e1a29cc
RitikRajput696 <rixusinput696@gmail.com>
2023-06-29 16:07:43 +0530

    added output selecors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // input buttons selector
-const inputTotalNumber = document.querySelector("#input-total-amount");
+const inputTotalAmount = document.querySelector("#input-total-amount");
 const btn5 = document.querySelector(".btn-5");
 const btn10 = document.querySelector(".btn-10");
 const btn15 = document.querySelector(".btn-15");
@@ -8,3 +8,9 @@ const btn50 = document.querySelector(".btn-50");
 const btnCustom = document.querySelector(".btn-custom");
 const tipBtns = document.querySelector(".btn-tip");
 const totalPeople = document.querySelector("#input-total-people");
+
+// output buttons selector 
+const outputPayableAmount = document.querySelector("#total-payable-amount");
+const tipPerPerson = document.querySelector("#tip-amount");
+const totalPerPerson = document.querySelector("#per-person");
+const resetBtn = document.querySelector(".reset");
